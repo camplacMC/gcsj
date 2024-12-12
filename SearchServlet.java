84d800d4d05b500d9209609dee8d2bedde5b39e9
1260170699 <kyy17313252217@outlook.com>
2024-12-12 17:27:20 +0800

    Create SearchServlet.java

---
+++ SearchServlet.java
@@ -0,0 +1,47 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.service.searchService;
+
+/**
+ * Servlet implementation class SearchServlet
+ */
+@WebServlet("/ProductSearchServlet")
+public class SearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SearchServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 在 Servlet 或 Controller 中的代码  // 获取搜索查询
+		String searchQuery = request.getParameter("searchQuery	");  
+		searchService ss=new searchService();
+		request.getRequestDispatcher("admin/product/search.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
