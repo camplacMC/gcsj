ad75878a8d50dc3a0cb3457c45dbbdf105403b94
1260170699 <kyy17313252217@outlook.com>
2024-12-12 17:27:14 +0800

    Create CheckServlet.java

---
+++ CheckServlet.java
@@ -0,0 +1,53 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.model.Student;
+import com.service.StudentService;
+
+/**
+ * Servlet implementation class EditStudentServlet
+ */
+@WebServlet("/CheckServlet")
+public class CheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		String Tid = request.getParameter("tid");
+		System.out.println("=="+Tid+"==");
+		StudentService ss = new StudentService();
+		Student student = ss.findByPid(Tid);
+		System.out.println(student);
+		request.setAttribute("student", student);
+		request.getRequestDispatcher("/admin/check.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
